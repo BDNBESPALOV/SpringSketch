e245ed1daf5dce58d86ac1c236847b229fabe8c5
Dmitry <bdn.bespalov@gmail.com>
2019-10-21 18:01:07 +0300

    11:42 Spring Boot Security

--- src/main/java/com/bdn/spring/GreetingController.java
+++ src/main/java/com/bdn/spring/GreetingController.java
@@ -17,20 +17,19 @@ public class GreetingController {
     @Autowired
     private MessageRepos messagesRepos;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World")
-                                       String name, Map<String, Object> model) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting( Map<String, Object> model) {
+
         return "greeting";
     }
-    @GetMapping
+    @GetMapping("main")
     public String main(Map<String ,Object> model){
         Iterable<Message> messages = messagesRepos.findAll();
     model.put("messages",messages);
     return "main";
     }
 
-    @PostMapping("add")
+    @PostMapping("main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
         Message message =new Message(text,tag);
         messagesRepos.save(message);
---
+++ src/main/java/com/bdn/spring/domain/Role.java
@@ -0,0 +1,5 @@
+package com.bdn.spring.domain;
+
+public enum Role {
+    USER;
+}
