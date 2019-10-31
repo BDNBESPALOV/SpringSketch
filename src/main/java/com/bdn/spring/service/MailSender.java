36dafee5f9fcf88e6043658ef09b27cd623eb82c
Dmitry <bdn.bespalov@gmail.com>
2019-10-31 11:24:14 +0300

    настроена email авторизация через smtp.yandex.ru (не протестирована)

--- src/main/java/com/bdn/spring/service/MailSender.java
+++ src/main/java/com/bdn/spring/service/MailSender.java
@@ -15,7 +15,7 @@ public class MailSender {
 
     public void send(String emailTo, String subject, String message){
         SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setFrom("");
+        mailMessage.setFrom(username);
         mailMessage.setTo(emailTo);
         mailMessage.setSubject(subject);
         mailMessage.setText(message);
