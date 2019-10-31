07304ae23632350e77f4e33c9a874134f1770ad3
Dmitry <bdn.bespalov@gmail.com>
2019-10-31 14:12:13 +0300

    добавлена миграция БД

--- src/main/java/com/bdn/spring/controller/userController.java
+++ src/main/java/com/bdn/spring/controller/userController.java
@@ -2,31 +2,30 @@ package com.bdn.spring.controller;
 
 import com.bdn.spring.domain.Role;
 import com.bdn.spring.domain.User;
-import com.bdn.spring.repos.UserRepo;
+import com.bdn.spring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
-@PreAuthorize("hasAuthority('ADMIN')")
 public class userController {
     @Autowired
-    private UserRepo userRepo;
+    private UserService userService;
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
     public String userList(Model model){
-        model.addAttribute("users",userRepo.findAll());
+        model.addAttribute("users",userService.findAll());
 
         return "userList";
     }
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("{user}")
     public String userEditForm(@PathVariable User user, Model model){
         model.addAttribute("user",user);
@@ -34,30 +33,32 @@ public class userController {
 
         return "userEdit";
     }
-
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping
     public String userSave(
             @RequestParam("userId") User user,
-            @RequestParam Map<String,Object> map,
+            @RequestParam Map<String,Object> form,
             @RequestParam String username
 
             ){
-        user.setUsername(username);
-        Set<String> roles = Arrays.stream(Role.values())
-                .map(Role:: name)
-                .collect(Collectors.toSet());
-        user.getRoles().clear();
-
-        for(String s : map.keySet()){
-            if(roles.contains(s)){
-                user.getRoles().add(Role.valueOf(s));
-
-            }
-
-        }
-
-        userRepo.save(user);
+        userService.saveUser(user,username,form);
 
         return "redirect:/user";
+    }
+    @GetMapping("profile")
+    public String getProfile(Model model, @AuthenticationPrincipal User user){
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email",user.getEmail());
+        return "profile";
+    }
+    @PostMapping("profile")
+    public String updateProfile(
+            @AuthenticationPrincipal User user,
+            @RequestParam String password,
+            @RequestParam String email
+    ){
+        userService.updateProfile(user,password,email);
+        return "redirect:/user/profile";
+
     }
 }
